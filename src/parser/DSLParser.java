24dbdd6bfa0621e9fa1bb4fb4667578666a23d3c
fauzanhilmi <fauzanhilmir@gmail.com>
2015-11-30 23:25:18 +0700

    parse kelas done

--- src/parser/DSLParser.java
+++ src/parser/DSLParser.java
@@ -12,32 +12,34 @@ import java.util.Scanner;
 public class DSLParser {
     private List<String> lines;
     private List<matkul> matkulList;
+    private List<kelas> kelasList;
     private int sksmax;
 
     public DSLParser() {
         lines = new ArrayList<>();
         matkulList = new ArrayList<>();
+        kelasList = new ArrayList<>();
+        sksmax = 0;
     }
 
     public void parse(String filename) throws FileNotFoundException {
         Scanner sc = new Scanner(new File(filename)).useDelimiter("\n");
-
         while(sc.hasNext()) {
             String s = sc.next();
             lines.add(s);
         }
         sc.close();
 
-
         for(int i=0; i<lines.size(); i++) {
             String currentLine = lines.get(i);
 
             //parse matkul
-            String nama = "";
-            String[] prereq = {};
-            int sks = 0;
-            boolean availability = false;
             if(currentLine.length()>=6 && currentLine.substring(0,6).equalsIgnoreCase("matkul")) {
+                String nama = "";
+                String[] prereq = {};
+                int sks = 0;
+                boolean availability = false;
+
                 nama = lines.get(i+1).split("( )*:( )*")[1].trim();
                 String sPre = lines.get(i+2).split("( )*:( )*")[1].trim();
                 prereq = sPre.split(" ");
@@ -57,17 +59,29 @@ public class DSLParser {
                 matkulList.add(m);
                 i+=4;
             }
-
             //parse jumlah sks
-            if(currentLine.length()>=6 && currentLine.substring(0,6).equalsIgnoreCase("sksmax")) {
+            else if(currentLine.length()>=6 && currentLine.substring(0,6).equalsIgnoreCase("sksmax")) {
                 sksmax = Integer.parseInt(currentLine.split("( )*:( )*")[1].trim());
-                System.out.println(sksmax);
+            }
+            //parse kelas
+            else if(currentLine.length()>=5 && currentLine.substring(0,5).equalsIgnoreCase("kelas")) {
+                String kode = "";
+                String matkul = "";
+                int kapasitas = 0;
+
+                kode = lines.get(i+1).split("( )*:( )*")[1].trim();
+                matkul = lines.get(i+2).split("( )*:( )*")[1].trim();
+                kapasitas = Integer.parseInt(lines.get(i+3).split("( )*:( )*")[1].trim());
+
+                kelas k = new kelas(kode,matkul,kapasitas);
+                kelasList.add(k);
+                i+=3;
             }
         }
-
         //TES
 //        for(matkul m: matkulList) System.out.println(m.nama+" "+m.sks+" "+m.prereq.length+" "+m.availability);
-
+//        System.out.println("sm = "+sksmax);
+//        for(kelas k: kelasList) System.out.println(k.kode+" "+k.matkul+" "+k.kapasitas);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
--- src/parser/Kelas.java
+++ src/parser/Kelas.java
@@ -4,4 +4,13 @@ package parser;
  * Created by Fauzan Hilmi on 30/11/2015.
  */
 public class kelas {
+    public String kode;
+    public String matkul;
+    public int kapasitas;
+
+    public kelas(String _kode, String _matkul, int _kapasitas) {
+        kode = _kode;
+        matkul = _matkul;
+        kapasitas = _kapasitas;
+    }
 }
